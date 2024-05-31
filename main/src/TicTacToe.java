fc390f7bf024640bb4d86415914f39d90f8c1d8b
abu166 <abulljk@gmail.com>
2024-06-01 01:34:58 +0500

    creating new void placePiece
    it places by checking if it plays human or computer based on this chooses where to place the specified sign, which are either O or X

--- main/src/TicTacToe.java
+++ main/src/TicTacToe.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
@@ -14,7 +15,18 @@ public class TicTacToe {
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter your placement (1-9) : ");
-        scan.nextInt();
+        int playerPos = scan.nextInt();
+
+        System.out.println(playerPos);
+
+        placePiece(gameBoard, playerPos, "player");
+
+        Random rand = new Random();
+        int cpuPos = rand.nextInt(9) + 1;
+        placePiece(gameBoard, cpuPos, "cpu");
+
+
+        printGameBoard(gameBoard);
 
     }
 
@@ -24,6 +36,46 @@ public class TicTacToe {
                 System.out.print(c);
             }
             System.out.println();
+        }
+    }
+
+    public static void placePiece(char[][] gameBoard, int pos, String user){
+        char symbol = ' ';
+        if(user.equals("player")){
+            symbol = 'X';
+        }else if(user.equals("cpu")){
+            symbol = 'O';
+        }
+        switch(pos){
+            case 1:
+                gameBoard[0][0] = symbol;
+                break;
+            case 2:
+                gameBoard[0][2] = symbol;
+                break;
+            case 3:
+                gameBoard[0][4] = symbol;
+                break;
+            case 4:
+                gameBoard[2][0] = symbol;
+                break;
+            case 5:
+                gameBoard[2][2] = symbol;
+                break;
+            case 6:
+                gameBoard[2][4] = symbol;
+                break;
+            case 7:
+                gameBoard[4][0] = symbol;
+                break;
+            case 8:
+                gameBoard[4][2] = symbol;
+                break;
+            case 9:
+                gameBoard[4][4] = symbol;
+                break;
+            default:
+                break;
         }
     }
 }
