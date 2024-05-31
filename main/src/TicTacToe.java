edc1428be257be039fb84885e5506a6f6db2d27d
abu166 <abulljk@gmail.com>
2024-06-01 01:17:42 +0500

    new void printGameBoard
    new void printGameBoard, prints gameBoard

--- main/src/TicTacToe.java
+++ main/src/TicTacToe.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class TicTacToe {
+    public static void main(String[] args){
+
+        char[][] gameBoard = {
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}};
+        printGameBoard(gameBoard);
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your placement (1-9) : ");
+        scan.nextInt();
+
+    }
+
+    public static void printGameBoard(char[][] gameBoard){
+        for(char[] row : gameBoard){
+            for(char c : row){
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
 }
